bb096312977ac345f297aeba1bf4d0b1da5ea585
racoonbest <91798907+racoonbest@users.noreply.github.com>
2022-02-09 00:06:24 -0500

    adding few other APIs

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
+
+    List<String> messages = new ArrayList<>();
 
     @RequestMapping("/user")
     public User getUser(@RequestParam(value = "pid") String pid, @RequestParam(value = "uid") String uid){
@@ -14,5 +19,18 @@ public class UserController {
         a.setPid(pid);
         a.setUid(uid);
         return a;
+    }
+
+    @RequestMapping("/tishqon")
+    public String getTishqon(@RequestParam(value = "pid") String input){
+
+        System.out.println(input);
+        messages.add(input);
+        return input;
+    }
+
+    @RequestMapping("/getAllText")
+    public List<String> add(){
+        return messages;
     }
 }
